b71a49b99f8d16afe9d9dcf8e046c229468800da
Stojan Trajkovski <stojantrajkovski06@gmail.com>
2026-09-06 15:01:57 +0800

    Added a subscriber to subscribe to rel_alt topic

--- src/probation_bringup/probation_bringup/gate_navigator.py
+++ src/probation_bringup/probation_bringup/gate_navigator.py
@@ -3,6 +3,8 @@ import rclpy
 from rclpy.node import Node
 
 from mavros_msgs.srv import SetMode
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 
 class GateNavigator(Node):
 
@@ -13,16 +15,25 @@ class GateNavigator(Node):
             self.get_logger().info('set_mode service not available, waiting again...')
         self.req = SetMode.Request()
         
+        # It seems that a relative alt of about -1.8 is roughly the middle of the gate
+        self.alt_sub = self.create_subscription(Float64, "mavros/global_position/rel_alt", self.alt_listener_callback, 10)
+        self.rel_alt = 0
+        
     def send_request(self):
         self.req.base_mode = 0
         self.req.custom_mode = "GUIDED"
         return self.set_mode_client.call_async(self.req)
+        
+    def alt_listener_callback(self, alt):
+        self.rel_alt = alt.data
              
         
 def main():
     rclpy.init()
 
     node = GateNavigator()
+    
+    # Set to GUIDED mode
     future = node.send_request()
     rclpy.spin_until_future_complete(node, future)
     response = future.result()
@@ -30,9 +41,16 @@ def main():
         node.get_logger().info("Entered GUIDED mode successfully")
     else:
         node.get_logger().warn("Failed to enter GUIDED mode")
+    
+    # Spin the node for everything else
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
 
     node.destroy_node()
-    rclpy.shutdown()
+    if rclpy.ok():
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
